c91f43faff0d91b130895f9981da38723f8d835b
cetmoko <gayatrimm17@gmail.com>
2025-01-16 11:17:18 +0530

    buttons work

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -53,23 +53,47 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame() {
-    for (i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(`Your choice: ${humanSelection}`);
-        console.log(`Computer choice: ${computerSelection}`);
-        console.log(playRound(humanSelection, computerSelection));
-        console.log(`Your Score: ${humanScore}`)
-        console.log(`Computer Score: ${computerScore}`)
-    }
     
-    if (computerScore > humanScore) {
-        console.log("Computer wins!")
-    } else if (computerScore < humanScore) {
-        console.log("You win!")
-    } else {
-        console.log("Draw!")
-    }
+    
+
+    
+    
+
+    // for (i = 0; i < 5; i++) {
+    //     const humanSelection = getHumanChoice();
+    //     const computerSelection = getComputerChoice();
+    //     console.log(`Your choice: ${humanSelection}`);
+    //     console.log(`Computer choice: ${computerSelection}`);
+    //     console.log(playRound(humanSelection, computerSelection));
+    //     console.log(`Your Score: ${humanScore}`)
+    //     console.log(`Computer Score: ${computerScore}`)
+    // }
+    
+    // if (computerScore > humanScore) {
+    //     console.log("Computer wins!")
+    // } else if (computerScore < humanScore) {
+    //     console.log("You win!")
+    // } else {
+    //     console.log("Draw!")
+    // }
 }
 
-playGame()
+// creating the ui
+const choices = document.querySelector("ul");
+const rock = document.createElement("button");
+rock.textContent = "Rock";
+
+const paper = document.createElement("button");
+paper.textContent = "Paper";
+
+const scissors = document.createElement("button");
+scissors.textContent = "Scissors";
+
+choices.appendChild(rock);
+choices.appendChild(paper);
+choices.appendChild(scissors);
+
+// getting human input and playing
+rock.addEventListener('click', function() {console.log(playRound('rock', getComputerChoice()))});
+paper.addEventListener('click', function() {console.log(playRound('paper', getComputerChoice()))});
+scissors.addEventListener('click', function() {console.log(playRound('scissors', getComputerChoice()))});
